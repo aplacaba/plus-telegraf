09dd8d2af0ca9f16fe36564f9f6c918f9978b693
aplacaba <alacaba@fastmail.com>
2023-08-13 19:40:40 +0800

    Added python import script and telegraf exec input

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 FROM telegraf:1.27-alpine
 
+# Install python/pip
+ENV PYTHONUNBUFFERED=1
+RUN apk add --update --no-cache python3 && ln -sf python3 /usr/bin/python
+
 COPY telegraf.conf /etc/telegraf/telegraf.conf
+COPY data_import.py /tmp/data_import.py
+
 
 EXPOSE 8125 8092 8094
 
---
+++ data_import.py
@@ -0,0 +1,46 @@
+from datetime import datetime, timedelta
+import os
+import pytz
+import requests
+
+API_KEY = os.environ.get("PHOTON_API_KEY")
+API_ENDPOINT = os.environ.get("PHOTON_API")
+INTERVAL = "1h"
+TIME_FORMAT = "%Y-%m-%d %R"
+
+def run_import():
+    now = datetime.utcnow()
+    params = {
+        'start': (now - timedelta(days=1)).strftime(TIME_FORMAT),
+        'end': now.strftime(TIME_FORMAT),
+        'interval': INTERVAL
+    }
+    headers = {'X-API-Key': API_KEY}
+    req = requests.get(API_ENDPOINT, headers=headers, params=params)
+    return req.json()
+
+
+def format_ts(timestamp):
+    naive = datetime.fromisoformat(timestamp)
+    local_dt = naive.astimezone(pytz.timezone("Asia/Manila"))
+    utc_dt = local_dt.astimezone(pytz.UTC)
+    return datetime.timestamp(utc_dt)
+
+
+def line_protocol(data):
+    return "photon_data,plant_no={plant_no} energy={energy},irradiance={irradiance} {timestamp}".format(
+        plant_no=data["plant_no"],
+        energy=data["power"],
+        irradiance=data["irradiance"],
+        timestamp=format_ts(data["log_timestamp"])
+    )
+
+
+def main():
+    result = run_import()
+    filtered = filter(lambda x: x.get("power") is not None, result["data"])
+    print("\n".join(list(map(line_protocol, filtered))))
+
+
+if __name__ == "__main__":
+    main()
